09ff966278f6d0b027e75cbb7a3a2bc48e87d162
antonbergenudd <antonbergenudd@hotmail.com>
2019-01-12 21:00:22 +0100

    Change image src to public folder

--- resources/views/product/default.blade.php
+++ resources/views/product/default.blade.php
@@ -1,6 +1,6 @@
 <div class="product" id="{{$product->id}}" style="width:calc(25% - 2rem); margin:1rem; height:20rem; min-width:20rem;">
     @if($product->thumbnail)
-        <img style="max-height: 60%; max-width:100%;" src="../images/games/{{$product->thumbnail}}" alt="{{$product->name}}">
+        <img style="max-height: 60%; max-width:100%;" src="{{ asset('img/games/'.$product->thumbnail) }}" alt="{{$product->name}}">
     @endif
     <h1 style="margin-bottom:0; margin-top:.5rem;">{{$product->name}} <span style="font-weight:400;">{{$product->price}} kr</span></h1>
     <p>{{$product->description}}</p>
