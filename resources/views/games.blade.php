411112868a512ef49e85f4a728c16680840e7723
antonbergenudd <antonbergenudd@hotmail.com>
2019-01-15 14:55:43 +0100

    change game layout

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <div class="flex-center" style="width:100%; height:40vh; background-color:black;">
-        <h1>Alla spel</h1>
-    </div>
-
-    <div class="" style="width:100%; display:flex; flex-wrap:wrap; padding:1rem;">
+    <div class="" style="margin-top:5rem; width:100%; display:flex; flex-wrap:wrap; padding:1rem;">
         <div style="display:flex; flex-wrap:wrap;">
             @foreach($products as $product)
                 @include('product.default')
