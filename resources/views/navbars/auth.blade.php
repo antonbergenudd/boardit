5441c2bed2b59c600787fcd8d712909fd536901a
antonbergenudd <antonbergenudd@hotmail.com>
2019-01-23 00:38:03 +0100

    CSS cleanup

--- resources/views/navbars/auth.blade.php
+++ resources/views/navbars/auth.blade.php
@@ -1,6 +1,6 @@
 <div class="navbar">
     <div class="left">
-        <img src="{{ asset('img/logo.png') }}" alt="" style="width:5rem; padding:1rem;">
+        <img class="logo-img" src="{{ asset('img/logo.png') }}" alt="Boarditgames logo">
     </div>
     <div class="links top-right">
         <a href="{{ url('/') }}">Home</a>
