b08a5a003ef80b7d033502aff3a145132fb904d0
antonbergenudd <antonbergenudd@hotmail.com>
2019-05-08 22:42:00 +0200

    Fix migrations

--- database/migrations/2019_01_09_133957_create_products_table.php
+++ database/migrations/2019_01_09_133957_create_products_table.php
@@ -20,7 +20,7 @@ class CreateProductsTable extends Migration
             $table->string('description');
             $table->string('thumbnail');
             $table->integer('price');
-            $table->integer('in_store')->default(1);
+            $table->integer('popular')->default(0);
         });
     }
 
