5080d6ee26b7f90584c8d846eebf0f8a08f0d733
antonbergenudd <antonbergenudd@hotmail.com>
2019-08-20 11:16:17 +0200

    Fixed login and register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<div class="container" style="padding:4rem;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
