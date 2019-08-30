db1c324226cd0408d6bce55d9d30b0abc37d9e5e
antonbergenudd <antonbergenudd@hotmail.com>
2019-08-30 21:35:38 +0200

    Fix fix fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -177,10 +177,8 @@ class MainController extends BaseController
             $messages = $client->messages->read(array(), 20);
             foreach ($messages as $i => $record) {
                 if(strpos($record->body, 'Referenskod') !== false) {
-                    Log::warning($record->body);
                     preg_match('/Referenskod:\s[0-9a-zA-Z]*/', $record->body, $matches);
-                    Log::warning($matches);
-                    $code = explode("Referenskod: ", $matches[0]);
+                    $code = explode("Referenskod: ", $matches[1]);
                     break;
                 }
             }
