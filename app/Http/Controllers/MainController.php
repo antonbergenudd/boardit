fd926a5bf13996b7dc6d189c423c0391b75bdcbb
antonbergenudd <antonbergenudd@hotmail.com>
2019-08-30 21:21:22 +0200

    test

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -177,7 +177,8 @@ class MainController extends BaseController
             $messages = $client->messages->read(array(), 20);
             foreach ($messages as $i => $record) {
                 if($i == 1) {
-                    preg_match('/referenskod:\s[0-9a-zA-Z]*/', $messages[1]->body, $matches);
+                    Log::warning($record->body);
+                    preg_match('/referenskod:\s[0-9a-zA-Z]*/', $record->body, $matches);
                     Log::warning($matches);
                     $code = explode("referenskod: ", $matches[0]);
                 }
