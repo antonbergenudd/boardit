9723be73a63ad7716a05b76d6cfe5da9a375ba05
antonbergenudd <antonbergenudd@hotmail.com>
2019-09-02 16:40:30 +0200

    whoop

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
                 {{-- <h1>Om </h1> --}}
                 <p>
                     Mitt namn är Anton Bergenudd och jag studerar på heltid på Karlstads universitet.
-                    Utbildningen jag studerar till heter Civilingenjör datateknik och varar i 5 år, varav jag är inne på år 2.
+                    Utbildningen jag studerar till heter Civilingenjör datateknik och varar i 5 år, varav jag är inne på år 3.
                     På fritiden spenderar jag det mesta av min tid till mina projekt som till exempel denna sida.
                     Med entreprenörskap som intresse har jag valt att starta den här sidan för att bedriva min egna verksamhet och se hur långt jag kan ta det.
                     Har jobbat som webbutvecklare i snart 3 år med glödande passion för ämnet.
