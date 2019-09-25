0258d4083031fed73ff718c9fbbea50816b20b01
antonbergenudd <antonbergenudd@hotmail.com>
2019-09-25 17:54:33 +0200

    Moved delivering to auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,24 @@
 
 namespace boardit\Http\Controllers;
 
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use boardit\User;
+
 class AuthController extends BaseController
 {
+    public function delivering(User $user, Request $request) {
+        $user->delivering = $request->delivering;
+
+        if($request->delivering) {
+            $user->delivering_since = Carbon::now()->format('Y-m-d H:i:s');
+        } else {
+            $user->delivering_since = NULL;
+        }
+
+        $user->save();
+
+        return back();
+    }
 }
