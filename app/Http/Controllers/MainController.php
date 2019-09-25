b9ace6d86951213dc33d8e189028c81d9771a413
antonbergenudd <antonbergenudd@hotmail.com>
2019-09-25 17:54:56 +0200

    Moved out functions to OrderController

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,246 +2,48 @@
 
 namespace boardit\Http\Controllers;
 
-use boardit\ProductOrder;
-
-use Twilio\Exceptions\TwilioException;
-
-use boardit\Jobs\ControlDelivering;
-
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Http\Request;
 use boardit\Product;
-use boardit\Order;
-use boardit\User;
-use boardit\Mail\ConfirmationMailable;
-use Cart;
-use Carbon\Carbon;
-
-use Illuminate\Support\Facades\Log;
-
-use Twilio\Rest\Client;
+use Gloudemans\Shoppingcart\Cart;
 
 class MainController extends BaseController
 {
     function index() {
         $products = Product::where('popular', 1)->get();
 
-        return view('index', compact('products'));
+        $cart = Cart::content();
+        $cartTotal = Cart::subTotal();
+
+        return view('index', compact('products', 'cart', 'cartTotal'));
     }
 
     function about() {
+        $cart = Cart::content();
+        $cartTotal = Cart::subTotal();
 
-        return view('about');
+        return view('about', compact('cart', 'cartTotal'));
     }
 
     function faq() {
-        return view('faq', compact('cart'));
+        $cart = Cart::content();
+        $cartTotal = Cart::subTotal();
+
+        return view('faq', compact('cart', 'cartTotal'));
     }
 
     function policy() {
-        return view('policy', compact('cart'));
+        $cart = Cart::content();
+        $cartTotal = Cart::subTotal();
+
+        return view('policy', compact('cart', 'cartTotal'));
     }
 
     function games() {
         $products = Product::where('show', 1)->get();
 
-        return view('games', compact('products'));
-    }
+        $cart = Cart::content();
+        $cartTotal = Cart::subTotal();
 
-    // MOVE TO AUTH CONTROLLER
-    public function orders() {
-        $orders = Order::all();
-        
-        return view('auth.orders', compact('orders'));
-    }
-
-    public function confirmOrder(User $user, Order $order, $redirect = true) {
-        $order->status = Order::CONFIRMED;
-        $order->confirmed_at = Carbon::now();
-        $order->delivered_at = NULL;
-        $order->user_id = $user->id;
-        $order->save();
-
-        try {
-            $this->notifyThroughSms($order);
-        } catch (TwilioException $e) {
-            echo  $e;
-        }
-
-        $this->email($order);
-
-        if($redirect) {
-            return back();
-        }
-    }
-
-    public function returnOrder(User $user, Order $order) {
-        $relationships = ProductOrder::where('order_id', $order->id)->get();
-
-        foreach($relationships as $relation) {
-            $relation->product->quantity++;
-            $relation->product->save();
-        }
-
-        $order->status = Order::RETURNED;
-        $order->error = 0;
-        $order->delivered_at = NULL;
-
-        $order->save();
-
-        return back();
-    }
-
-    public function deliverOrder(User $user, Order $order) {
-        $order->status = Order::DELIVERED;
-        $order->delivered_at = Carbon::now();
-        $order->confirmed_at = NULL;
-        $order->error = 0;
-
-        $order->save();
-
-        return back();
-    }
-
-    public function delivering(User $user, Request $request) {
-        $user->delivering = $request->delivering;
-
-        if($request->delivering) {
-            $user->delivering_since = Carbon::now()->format('Y-m-d H:i:s');
-        } else {
-            $user->delivering_since = NULL;
-        }
-
-        $user->save();
-
-        return back();
-    }
-
-    private function email($data) {
-        try {
-            Mail::to($data->email)
-                ->send(new ConfirmationMailable($data));
-        } catch(\Exception $e) {
-            dd($e);
-        }
-    }
-
-    private function notifyThroughSms($order)
-    {
-        if(env('SEND_SMS')) {
-            $this->sendSms(
-                $order->phone,
-                "Din order är bekräftad!" .
-                "\r\nReferenskod: " . $order->code .
-                "\r\nVäntad leveranstid ". Carbon::now('Europe/Stockholm')->addHours('2')->format('H:i') .
-                "\r\nMvh, Boarditgames.\r\nTack för att ni valde oss!"
-            );
-        }
-    }
-
-    protected function sendSms($to, $message)
-    {
-        $accountSid = env('TWILIO_ACCOUNT_SID');
-        $authToken = env('TWILIO_AUTH_TOKEN');
-
-        $client = new Client($accountSid, $authToken);
-
-        try {
-            $client->messages->create(
-                $to,
-                [
-                    "body" => $message,
-                    "from" => env('TWILIO_NUMBER')
-                ]
-            );
-        } catch (TwilioException $e) {
-            echo  $e;
-        }
-    }
-
-    public function receiveSms()
-    {
-        $accountSid = env('TWILIO_ACCOUNT_SID');
-        $authToken = env('TWILIO_AUTH_TOKEN');
-
-        $client = new Client($accountSid, $authToken);
-
-        /*
-        ** Default response message when receiving a message without key words.
-        */
-        $defaultMessage = "Svara JA om du vill acceptera uppdraget.";
-
-        /*
-        ** Read the contents of the incoming message fields.
-        */
-        $body = $_REQUEST['Body'];
-        $to = $_REQUEST['From'];
-        $from = $_REQUEST['To'];
-
-        /*
-        ** Remove formatting from $body until it is just lowercase
-        ** characters without punctuation or spaces.
-        */
-        $response = preg_replace("/[^A-Za-z0-9]/u", " ", $body);
-        $response = trim($response);
-        $response = strtolower($response);
-        $sendDefault = true; // Default message is sent unless key word is found in following loop.
-
-        /*
-        ** Choose the correct message response and set default to false.
-        */
-        if ($response == 'ja') {
-
-            // Retrieve old messages to get last sent code
-            $messages = $client->messages->read(array(), 20);
-            foreach ($messages as $i => $record) {
-                if(strpos($record->body, 'Referenskod') !== false) {
-                    preg_match('/Referenskod:\s[0-9a-zA-Z]*/', $record->body, $matches);
-                    $code = explode("Referenskod: ", $matches[0])[1];
-                    break;
-                }
-            }
-
-            // Retrieve order from DB
-            $order = Order::where('code', $code)->first();
-
-            // Check if already confirmed
-            if(!$order->confirmed) {
-                $body = 'Du har accepterat uppdraget.';
-
-                $nr = str_replace("+46", "0", $to);
-                $user = User::where('phone', $nr)->first();
-
-                $this->confirmOrder($user, $order, false);
-            } else {
-                $body = 'Uppdraget är redan taget.';
-            }
-
-            $sendDefault = false;
-        }
-
-        // Send the correct response message.
-        $client->messages->create(
-            $to,
-            array(
-                'from' => $from,
-                'body' => $sendDefault ? $defaultMessage : $body,
-            )
-        );
-    }
-
-    public function controlOrder(Request $request) {
-        $order = Order::where('id', $request->order)->first();
-
-        if($order->status == Order::CONFIRMED && Carbon::parse($order->confirmed_at)->addHours('2') < Carbon::now()) {
-            $order->error = 1;
-        }
-
-        if($order->status == Order::DELIVERED && Carbon::parse($order->delivered_at)->addDays('1') < Carbon::now()) {
-            $order->error = 1;
-        }
-
-        $order->save();
+        return view('games', compact('products', 'cart', 'cartTotal'));
     }
 }
