e9a2edf84eb9e1979cfeeb7b5d27efa500cd1ffd
antonbergenudd <antonbergenudd@hotmail.com>
2019-10-29 23:40:51 +0100

    More orderController tests

--- tests/Unit/OrderControllerTest.php
+++ tests/Unit/OrderControllerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Stripe\Customer;
+
 use boardit\User;
 use boardit\Product;
 use boardit\Order;
@@ -15,143 +17,160 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
 
+use JacobBennett\StripeTestToken;
+use Stripe\Charge;
+
 class OrderControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Test index.
+     *
+     * @return void
+     */
+    public function testIndex() {
+        $response = $this->get('/auth/orders');
+        $response->assertStatus(302);
+
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+        $response = $this->get('/auth/orders');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test status.
+     *
+     * @return void
+     */
+    public function testStatus() {
+        $class = new OrderController;
+
+        $order = factory(Order::class)->create(['status' => Order::CONFIRMED]);
+        $this->assertEquals("true", $class->status($order));
+
+        $order = factory(Order::class)->create(['status' => Order::PROCESSING]);
+        $this->assertEquals("false", $class->status($order));
+    }
+
     // /**
-    //  * Test index.
+    //  * Test quantity.
     //  *
     //  * @return void
     //  */
-    // public function testIndex() {
-    //     $response = $this->get('/auth/orders');
-    //     $response->assertStatus(302);
-    //
-    //     $user = factory(User::class)->create();
-    //
-    //     $this->actingAs($user);
-    //
-    //     $response = $this->get('/auth/orders');
-    //     $response->assertStatus(200);
-    // }
-    //
-    // /**
-    //  * Test status.
-    //  *
-    //  * @return void
-    //  */
-    // public function testStatus() {
-    //     $class = new OrderController;
-    //
-    //     $order = factory(Order::class)->create(['status' => Order::CONFIRMED]);
-    //     $this->assertEquals("true", $class->status($order));
-    //
-    //     $order = factory(Order::class)->create(['status' => Order::PROCESSING]);
-    //     $this->assertEquals("false", $class->status($order));
-    // }
-    //
-    // // /**
-    // //  * Test quantity.
-    // //  *
-    // //  * @return void
-    // //  */
-    // // public function testQuantity() {
-    // //     // Cart test
-    // // }
-    //
-    // /**
-    //  * Test set failed.
-    //  *
-    //  * @return void
-    //  */
-    // public function testSetFailed() {
-    //     $class = new OrderController;
-    //
-    //     $order = factory(Order::class, 1)->create(['status' => Order::CONFIRMED])
-    //          ->each(function($u) {
-    //              $u->getProducts()->save(factory(Product::class)->make());
-    //          })->first();
-    //
-    //     $class->setFailed($order);
-    //
-    //     foreach($order->getProducts as $product) {
-    //         $this->assertTrue($product->quantity > 0);
-    //     }
-    //
-    //     $this->assertDatabaseHas('orders', [
-    //         'id' => $order->id,
-    //         'status' => Order::FAILED,
-    //         'error' => 1
-    //     ]);
-    // }
-    //
-    // public function testNotifyOffline() {
-    //     $accountSid = env('TWILIO_ACCOUNT_SID_TEST');
-    //     $authToken = env('TWILIO_AUTH_TOKEN_TEST');
-    //
-    //     $client = new Client($accountSid, $authToken);
-    //
-    //     $class = new OrderController;
-    //
-    //     $order = factory(Order::class)->create([
-    //         'deliverance_date' => Carbon::now('Europe/Stockholm')->addHours('2'),
-    //     ]);
-    //
-    //     $class->notifyOffline($order);
-    //
-    //     $this->assertEquals("delivered", $client->messages->read(array(), 1)[0]->status);
-    // }
-    //
-    // // TODO: How to test stripe in unit test
-    // // public function testConfirm() {
-    // //     $class = new OrderController;
-    // //
-    // //     $order = factory(Order::class)->create([
-    // //         'deliverance_date' => Carbon::now('Europe/Stockholm')->addHours('2'),
-    // //     ]);
-    // //
-    // //     $user = factory(User::class)->create();
-    // //
-    // //     $class->confirm($user, $order, false);
-    // //
-    // //     $this->assertEquals(Order::CONFIRMED || Order::CONFIRMED_AND_RESERVED, $order->status);
-    // // }
-    //
-    // public function testReturn() {
-    //     $class = new OrderController;
-    //
-    //     $user = factory(User::class)->create();
-    //     $order = factory(Order::class, 1)->create(['status' => Order::CONFIRMED])
-    //          ->each(function($u) {
-    //              $u->getProducts()->save(factory(Product::class)->create(['quantity' => 0]));
-    //          })->first();
-    //
-    //
-    //     $class->return($user, $order);
-    //
-    //     $this->assertEquals(Order::RETURNED, $order->status);
-    //     $this->assertEquals(0, $order->error);
-    //     $this->assertEquals(1, $order->getProducts()->first()->quantity);
-    // }
-    //
-    // public function testDeliver() {
-    //     $class = new OrderController;
-    //
-    //     $user = factory(User::class)->create();
-    //     $order = factory(Order::class, 1)->create()
-    //          ->each(function($u) {
-    //              $u->getProducts()->save(factory(Product::class)->create(['quantity' => 0]));
-    //          })->first();
-    //
-    //     $class->deliver($user, $order);
-    //
-    //     $this->assertEquals(Order::DELIVERED, $order->status);
-    //     $this->assertEquals(0, $order->error);
-    //     $this->assertNotEquals(null, $order->delivered_at);
+    // public function testQuantity() {
+    //     // Cart test
     // }
 
-    // TODO: How to test twilio webhook
+    /**
+     * Test set failed.
+     *
+     * @return void
+     */
+    public function testSetFailed() {
+        $class = new OrderController;
+
+        $order = factory(Order::class, 1)->create(['status' => Order::CONFIRMED])
+             ->each(function($u) {
+                 $u->getProducts()->save(factory(Product::class)->make());
+             })->first();
+
+        $class->setFailed($order);
+
+        foreach($order->getProducts as $product) {
+            $this->assertTrue($product->quantity > 0);
+        }
+
+        $this->assertDatabaseHas('orders', [
+            'id' => $order->id,
+            'status' => Order::FAILED,
+            'error' => 1
+        ]);
+    }
+
+    public function testNotifyOffline() {
+        $accountSid = env('TWILIO_ACCOUNT_SID_TEST');
+        $authToken = env('TWILIO_AUTH_TOKEN_TEST');
+
+        $client = new Client($accountSid, $authToken);
+
+        $class = new OrderController;
+
+        $order = factory(Order::class)->create([
+            'deliverance_date' => Carbon::now('Europe/Stockholm')->addHours('2'),
+        ]);
+
+        $class->notifyOffline($order);
+
+        $this->assertEquals("delivered", $client->messages->read(array(), 1)[0]->status);
+    }
+
+    //TODO: How to test stripe in unit test
+    public function testConfirm() {
+        $class = new OrderController;
+
+        StripeTestToken::setApiKey(env('STRIPE_TEST_KEY'));
+
+        // Success card
+        $customer = Customer::create([
+            'source' => StripeTestToken::validVisa(),
+        ]);
+
+        $order = factory(Order::class,1)->create([
+            'deliverance_date' => Carbon::now('Europe/Stockholm')->addHours('2'),
+            'payment_token' => $customer->id,
+            'payment' => 100,
+            'phone' => '+46708605003'
+        ])->each(function($u) {
+            $u->getProducts()->save(factory(Product::class)->make());
+        })->first();
+
+        $user = factory(User::class)->create();
+
+        $class->confirm($user, $order, false);
+
+        $this->assertEquals(Order::CONFIRMED || Order::CONFIRMED_AND_RESERVED, $order->status);
+        $this->assertEquals($user->id, $order->user_id);
+        $this->assertNotEquals(null, $order->confirmed_at);
+    }
+
+    public function testReturn() {
+        $class = new OrderController;
+
+        $user = factory(User::class)->create();
+        $order = factory(Order::class, 1)->create(['status' => Order::CONFIRMED])
+             ->each(function($u) {
+                 $u->getProducts()->save(factory(Product::class)->create(['quantity' => 0]));
+             })->first();
+
+
+        $class->return($user, $order);
+
+        $this->assertEquals(Order::RETURNED, $order->status);
+        $this->assertEquals(0, $order->error);
+        $this->assertEquals(1, $order->getProducts()->first()->quantity);
+    }
+
+    public function testDeliver() {
+        $class = new OrderController;
+
+        $user = factory(User::class)->create();
+        $order = factory(Order::class, 1)->create()
+             ->each(function($u) {
+                 $u->getProducts()->save(factory(Product::class)->create(['quantity' => 0]));
+             })->first();
+
+        $class->deliver($user, $order);
+
+        $this->assertEquals(Order::DELIVERED, $order->status);
+        $this->assertEquals(0, $order->error);
+        $this->assertNotEquals(null, $order->delivered_at);
+    }
+
+    // // TODO: How to test twilio webhook
     // public function testReceiveSms() {
     //     $class = new OrderController;
     //
