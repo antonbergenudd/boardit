c613ba9aa9735898dea39d94659954e5d2357acf
antonbergenudd <antonbergenudd@hotmail.com>
2019-12-02 10:32:20 +0100

    push validation function

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/order/validate', 'OrderController@validateOrder');
+Route::post('/order/push', 'OrderController@validatePush');
